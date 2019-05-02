9a6a2dc674cdd357c62dcdfa2429eceab653c470
soflyd <soflyd@users.noreply.github.com>
2019-05-02 23:00:36 +0200

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,22 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'demo';
+  title = 'Calculator App';
+  num1: number;
+  num2: number;
+  result: number;
+  add() {
+    this.result = this.num1 + this.num2;
+  }
+  substract() {
+    this.result = this.num1 - this.num2;
+  }
+  multiply() {
+    this.result = this.num1 * this.num2;
+  }
+  divide() {
+    this.result = this.num1 % this.num2;
+  }
 }
